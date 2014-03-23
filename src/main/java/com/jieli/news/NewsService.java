b71f568a8fe638c3614e028e7493b64c9d3b117b
liangjun.zhong <zhongliangjun1@gmail.com>
2014-03-23 23:45:30 +0800

    add service

--- src/main/java/com/jieli/news/NewsService.java
+++ src/main/java/com/jieli/news/NewsService.java
@@ -1,10 +1,14 @@
 package com.jieli.news;
 
+import com.jieli.activity.Activity;
 import com.jieli.mongo.GenericDAO;
 import com.jieli.user.dao.UserDAO;
+import com.jieli.util.IdentifyUtils;
 import com.sun.jersey.spi.resource.Singleton;
 
-import javax.ws.rs.Path;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 /**
  * Created with IntelliJ IDEA.
@@ -24,7 +28,21 @@ public class NewsService {
     private final UserDAO userDAO = new UserDAO();
 
 
-    //private
+    @GET
+    @Path("/")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response findNews(@CookieParam("u")String userId, @QueryParam("type") String type, @QueryParam("page") int page, @QueryParam("pagesize") int pagesize){
+
+        if (!IdentifyUtils.isValidate(userId)) {
+            return Response.status(403).build();
+        }
+
+
+
+
+
+        return null;
+    }
 
 
 
